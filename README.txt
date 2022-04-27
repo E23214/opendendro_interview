875a4e2c1dcdc0eaf15bdb0402a704d4ef69c544
E23214 <elliotarlo@gmail.com>
2022-04-27 15:44:51 -0700

    Final Version

---
+++ README.txt
@@ -0,0 +1,53 @@
+To analyze CSV data, place the file you want in the folder CSV_DATA. Then, call the python program from 
+the command line with the name of the file you want to analyze. For the statistics summary,
+enter the number of columns you want to analyze. If you want a full stats summary (similar to that given in
+the R example) also add "stats"
+example commandline argument using files already existing in the folder:
+python3 CSV_Tree.py TTHH_raw.csv 3
+
+To end the program, exit out of the graph generated.
+
+The program works by parsing the csv file into the pandas DataFrame data structure. This section mainly
+explains how the pandas library is working (or at least my understanding of how it working). I apologize
+if I both under and over explain, as this is the first time I am using the pandas library. Instead of 
+going through each line, I'm going to try and explain the things necesarry to understand the other
+parts of the code and avoid the more self explanatory parts.
+
+For the stats summary, most use the pandas dataframe methods (e.g., DataFrame[Column_Name].std() 
+ give the std deviation of the number values in the column) to get the result. To find the first year
+ where data occurs I used data[i].index.get_loc(data[i].first_valid_index()) to find the index of the row
+ of the first and just put the data.at[index_of_first_valid_input, 'Year'] to find the year of that first 
+ instance.
+
+For the full stats, it's similar. To find the total numver of valid inputs, 
+ I used (data.shape[0])-data.isna().sum()).mean(). In order to explain this, I need to the way the pandas dataframe
+ methods work.Many of the dataframe methods (e.g., .mean()) return a dataframe with 
+ the first column being column names and then the performed operation as the second column.
+ So, the operations where I do .sum().sum() returns the total sum of whatever dataframe I am referring 
+ to. .isna() returns a dataframe where the invalid inputs (non numbers) 
+ are True and valid inputs are False. data.isna().sum().sum() retruns the total number of invalid inputs.
+ data.shape[0] gives the number of rows, data.shape[1] gives the number of columns. So, the whole
+ statement "data.shape[0])-data.isna().sum()).mean()" gives the total number of valid inputs.
+For the graph, I copied it almost whole cloth from a post about making a spaghetti graph.
+    palette = plt.cm.twilight(np.linspace(0, 1, len(data.columns)+1))
+    This line generates a color palette with using matplotlib using matplotlib.cm.twilight() method.
+    I changed the color palette from rainbow to twilight because I thought it was slightly more readable
+        The numpy.linspace((0, 1, len(data.columns)+1)) generates (len(data.columns)+1) equally spaced values
+        between 0 and 1
+    The line of code:\
+        plt.plot(data[xAxis], data[column], marker='', linewidth=.75, alpha=0.9, label=column, color = palette[num])
+        uses the matplotlib.plot method instead of the pandas dataframe.plot() since the pandas one made individual
+        graphs for each one. I used data[xAxis] to make it be able to read ANY csv file (but the rest of my code doesnt so
+        kind of a moot point)
+    The legend needed to be sort of dynamic so the arguemnt loc='best' places it in the location which
+    minimizees the overlap with the lines on the graph. If I were to iterate on it, I would make the ncol
+    dynamic to the number of columns
+         plt.legend(loc='best', ncol=6, markerscale=.5,fontsize='small')
+To make the program work better with other csv files I used dataframe.columns[0] instead of "Year" so that 
+it doesn't need the same capitalization. 
+To make the requirements.txt, I found this 
+https://stackoverflow.com/questions/31684375/automatically-create-requirements-txt 
+    So, I just put in the command line
+    pip install pipreqs
+
+    pipreqs /path/to/project
--- python/CSV_Tree.py
+++ python/CSV_Tree.py
@@ -1,26 +1,84 @@
-import csv
-import matplotlib
+
+import matplotlib.pyplot as plt
 import sys
 import pandas
+import numpy as np
 import os
 
-def CSV_Parser():
-        with open("./CSV DATA/TTHH_raw.csv") as csv_file:
-            columns = []
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
-            for row in csv_reader:
-                if line_count == 0:
-                    columns = row
-                    line_count+=1
-                else:
-                    print(line_count,end=',')
-                    line_count+=1
 
+def CSV_Parser():   
+        directory =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        if(len(sys.argv) == 1):#Basically, the default file argument
+           CSV_data = pandas.read_csv(os.path.join(directory,"CSV_DATA/wa082.csv"))
+        else:#user would put the custom 
             
-        if(len(sys.argv) == 1):
-           CSV_data = pandas.read_csv("./CSV DATA/TTHH_raw.csv")
-        else:
-            CSV_data = pandas.read_csv(sys.argv[1])
-        print(CSV_data.columns)
-CSV_Parser()
+            CSV_data = pandas.read_csv(os.path.join(directory,"CSV_DATA",sys.argv[1]))
+
+        return CSV_data
+def stats(data):#Full stats
+        print(data.columns[1:]) #Prints the different columns, excluding the year
+        print(f"Number of dated series:{len(data.columns)-1}")
+        #will be abusing panda's datafram maniulation for the next few lines
+        print(f"Avg series length: {((data.shape[0])-data.isna().sum()).mean()}")
+        #Shape[0] gives the num of rows, isna() is the same format as data just with True where the
+        #data is not a number. So, .sum gives the number of non-numbers in each column. 
+        numDataPoints = data.shape[1]*data.shape[0]-data.isna().sum().sum() #BxH - a sum of a sum of the NAN values in each column
+        print(f"Number of data points: {numDataPoints}")
+        print(f"Some kind of avg std deviation {data.std().mean()}")
+
+        for i in data.columns[1:]:
+            idx = data[i].isnull()
+            it = 0
+            print(i+" years with missing data -- ",end='')
+            for n in idx.array:
+                if(n == True):
+                    print(data.at[it,data.columns[0]],end=',')
+                it+=1
+            print()
+            print()
+def stats_summary(data, nums):
+    print(f'{"##   series first last year  mean  median  stdev   skew":20}')
+    row = 0
+    for i in data.columns[1:]:
+        row+=1
+        ##https://stackoverflow.com/questions/22403469/locate-first-and-last-non-nan-values-in-a-pandas-dataframe to find inital year
+        first = data.at[data[i].index.get_loc(data[i].first_valid_index()),data.columns[0]]
+        last = data.at[data[i].index.get_loc(data[i].last_valid_index()),data.columns[0]]
+        print(f"{f'## {row} {i} {first}  {last}      {data[i].mean():.4f} {data[i].median():.4f} {data[i].std():.4f} {data[i].skew():.4f}':20}")
+        if row == nums:
+            break
+
+def Spaghetti_Plot(data,Selected_Range = 0, Selected_Columns = []):
+    # Change the style of plot
+       #Copied from here https://python-graph-gallery.com/124-spaghetti-plot
+        plt.style.use('seaborn-darkgrid')
+        # Create a color palette
+        palette = plt.cm.twilight(np.linspace(0, 1, len(data.columns)+1))
+        # Plot multiple  lines
+        num = 0
+        for column in data.columns:
+            if num == 0:
+                num +=1
+                xAxis = column
+            else:
+                plt.plot(data[xAxis], data[column], marker='', linewidth=.75, alpha=0.9, label=column, color = palette[num])
+                num+=1
+        # Add legend
+        plt.legend(loc='best', ncol=6, markerscale=.5,fontsize='small')
+        
+        # Add titles
+        plt.title(f"Spaghetti Plot of {xAxis[0]}", loc='left', fontsize=12, fontweight=0, color='orange')
+        plt.xlabel(data.columns[0])
+        plt.ylabel("Not Sure what this data represents (tree ring series?)")
+        plt.show()
+if __name__ == '__main__':
+    fData = CSV_Parser()
+    length = 5
+    for i in range(len(sys.argv)):
+        if(sys.argv[i].isnumeric()):
+            length = int(sys.argv[i])
+        if sys.argv[i] == "stats":
+            stats(fData)
+    stats_summary(fData,length)
+    Spaghetti_Plot(fData)
+    exit
